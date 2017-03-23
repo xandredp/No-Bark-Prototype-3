aa4d545574922bcd4540891dca885eaf46ae919c
xandredp <xduplooy@gmail.com>
2017-03-24 05:40:47 +1300

    First commit

---
+++ Source/NoBark_Prototype3/NoBark_Prototype3GameModeBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "NoBark_Prototype3GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NOBARK_PROTOTYPE3_API ANoBark_Prototype3GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
