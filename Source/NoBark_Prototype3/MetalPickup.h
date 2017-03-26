26e2a43fbc4aca2605d189ce1bb3c41cd6966260
Sena <love-diving@hotmail.com>
2017-03-27 01:22:22 +1300

    modified pickup classes

--- Source/NoBark_Prototype3/MetalPickup.h
+++ Source/NoBark_Prototype3/MetalPickup.h
@@ -13,7 +13,21 @@ class NOBARK_PROTOTYPE3_API AMetalPickup : public APickup
 {
 	GENERATED_BODY()
 	
-	
+
+public:
+	AMetalPickup();
+
+	/** Override the WasCollected function - use Implementation because it's a Blueprint Native Event */
+	void WasCollected_Implementation() override;
+	/**Public way to access the battery's power level */
+	float GetPower();
+
+protected:
+	/**Set the amount of power the battery gives to the character */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Power", Meta = (BlueprintProtected = "true"))
+		float MetalPickupPower;
+
 	
 	
 };
+
