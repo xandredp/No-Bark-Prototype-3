012067790c4ff620703f7b9efde6b23f79bfc2b6
Sena <love-diving@hotmail.com>
2017-03-29 22:46:07 +1300

    intermediate saved source

---
+++ Source/NoBark_Prototype3/GameController.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/PlayerController.h"
+#include "GameController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NOBARK_PROTOTYPE3_API AGameController : public APlayerController
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
