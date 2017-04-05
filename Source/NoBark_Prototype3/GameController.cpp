6e8355355d9ded0ff7f386810ff8244255bd858b
Sena <love-diving@hotmail.com>
2017-04-06 10:10:12 +1200

    removewhole

--- Source/NoBark_Prototype3/GameController.cpp
+++
@@ -1,8 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "NoBark_Prototype3.h"
-#include "GameController.h"
-
-
-
-
--- Source/NoBark_Prototype3/NoBark_Prototype3.h
+++
@@ -1,8 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#pragma once
-
-#include "Engine.h"
-
-#define COLLISION_INTERACTABLE ECC_GameTraceChannel1;
-
